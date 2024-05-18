56c2b12759e62246f92a27eef4b75aad9d8c6a31
williammasivi <williammasivi@gmail.com>
2024-05-18 13:31:13 +0200

    starter-code

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+
+
+function App() {
+   return (
+      <h1>Banking Full Stack Website</h1>
+   )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
+   <App />
+)
